c48d1ee02178491179bb9c13af25366327554c86
mborne <mborne@users.noreply.github.com>
2023-09-16 12:28:26 +0200

    rclone - upgrade

--- rclone/install.sh
+++ rclone/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# https://downloads.rclone.org/version.txt
+VERSION=${VERSION:-1.64.0}
+
 echo "-------------------------------------------------------------------"
 echo "-- rclone/install.sh - v$VERSION"
 echo "-------------------------------------------------------------------"
 
-VERSION=${VERSION:-1.62.2}
 ARCH=$(dpkg --print-architecture)
 URL=https://downloads.rclone.org/v${VERSION}/rclone-v${VERSION}-linux-${ARCH}.deb
 
 wget -nc -O /tmp/rclone-v${VERSION}-linux-${ARCH}.deb $URL
-dpkg -i /tmp/rclone-v${VERSION}-linux-${ARCH}.deb
+sudo dpkg -i /tmp/rclone-v${VERSION}-linux-${ARCH}.deb
