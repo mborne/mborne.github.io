f04dd781a97bdcd120c9ee4267d119421914db11
mborne <mborne@users.noreply.github.com>
2023-10-08 10:37:34 +0200

    ansible - add passlib install

--- ansible/install.sh
+++ ansible/install.sh
@@ -4,4 +4,7 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
 sudo apt-get install -y ansible
 
+# required to use {{ registry_admin_password | password_hash('bcrypt') }}
+pip3 install passlib --user
+
 
