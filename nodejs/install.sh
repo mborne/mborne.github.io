63aa5ce3516c58db474948e6b10013ef3185bde1
mborne <mborne@users.noreply.github.com>
2023-11-20 21:39:20 +0100

    feat(nodes): upgrade

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-# https://github.com/nodesource/distributions#debmanual
+# from https://deb.nodesource.com/node_20.x
 
-VERSION=${VERSION:-18}
+NODE_MAJOR=${NODE_MAJOR:-20}
 
 echo "-------------------------------------------------------------------"
-echo "-- nodejs/install.sh - $VERSION ..."
+echo "-- nodejs/install.sh - ${NODE_MAJOR}.x ..."
 echo "-------------------------------------------------------------------"
 
-KEYRING=/usr/share/keyrings/nodesource.gpg
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | sudo tee "$KEYRING" >/dev/null
-sudo chmod a+r /usr/share/keyrings/nodesource.gpg
+sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg -y --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
-DISTRO="$(lsb_release -s -c)"
-echo "deb [signed-by=$KEYRING] https://deb.nodesource.com/node_${VERSION}.x $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src [signed-by=$KEYRING] https://deb.nodesource.com/node_${VERSION}.x $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
-
-sudo apt-get update
-sudo apt-get install -y nodejs
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update && sudo apt-get install nodejs -y
 
 echo "-------------------------------------------------------------------"
 echo "-- node : $(node --version)"
