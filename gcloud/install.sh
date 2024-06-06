421047a0e14078530163203a148de2bf986f85eb
mborne <mborne@@users.noreply.github.com>
2024-06-07 00:38:31 +0200

    chore(gcloud): ajout plugin authent. GKE

--- gcloud/install.sh
+++ gcloud/install.sh
@@ -13,4 +13,4 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor 
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 
 sudo apt-get update
-sudo apt-get install -y google-cloud-cli
+sudo apt-get install -y google-cloud-cli google-cloud-sdk-gke-gcloud-auth-plugin
