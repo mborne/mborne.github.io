c56e7e9e0643ba27aad217e9b08aa6decdfc1faa
mborne <mborne@users.noreply.github.com>
2024-06-08 11:19:48 +0200

    fix(nodejs): fix install.sh

--- nodejs/install.sh
+++ nodejs/install.sh
@@ -9,7 +9,7 @@ echo "-- nodejs/install.sh - ${NODE_MAJOR}.x ..."
 echo "-------------------------------------------------------------------"
 
 sudo apt-get update && sudo apt-get install -y ca-certificates curl gnupg
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg -y --dearmor -o /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt-get update && sudo apt-get install nodejs -y
