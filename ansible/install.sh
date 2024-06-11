17d8f590aa6bc0c27f9c18d0166b8f0efd80228a
mborne <mborne@users.noreply.github.com>
2024-06-11 23:37:23 +0200

    fix(ansible): missing python3-pip

--- ansible/install.sh
+++ ansible/install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository --yes --update ppa:ansible/ansible
-sudo apt-get install -y ansible
+sudo apt-get install -y python3-pip ansible
 
 # required to use {{ registry_admin_password | password_hash('bcrypt') }}
 pip3 install passlib --user
