4bb3b311885bed38ac6b9d3dec76bda940eeb065
mborne <mborne@users.noreply.github.com>
2026-03-16 23:01:31 +0100

    chore(kind): install jq and curl if missing (closes #14)

--- docs/outils/kind/install.sh
+++ docs/outils/kind/install.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
+ICON_INFO=$(printf "ℹ️ \x08 ")
+ICON_WARN=$(printf "⚠️ \x08 ")
+ICON_ERROR=$(printf "❌ \x08 ")
+ICON_OK=$(printf "✅ \x08 ")
+
+VERSION=${VERSION:-0.31.0}
+
+echo "-------------------------------------------------------------------"
+echo "-- kind/install.sh - v$VERSION"
+echo "-------------------------------------------------------------------"
+
 if ! command -v curl &> /dev/null
 then
-    echo "curl is required (sudo apt-get update && sudo apt-get install -y curl)"
-    exit 1
+    echo "${ICON_INFO} curl is required, installing it (sudo apt-get update && sudo apt-get install -y curl)"
+    sudo apt-get update && sudo apt-get install -y curl
 fi
 
 if ! command -v jq &> /dev/null
 then
-    echo "jq is required (sudo apt-get update && sudo apt-get install -y jq)"
-    exit 1
+    echo "${ICON_INFO}jq is required, installing it (sudo apt-get update && sudo apt-get install -y jq)"
+    sudo apt-get update && sudo apt-get install -y jq   
 fi
 
-VERSION=${VERSION:-0.30.0}
+# Ensure that the last version of kind is installed
 LAST_VERSION=$(curl --silent "https://api.github.com/repos/kubernetes-sigs/kind/releases/latest" | jq -r .tag_name)
+if [ "v$VERSION" != "$LAST_VERSION" ]; then
+    echo "${ICON_WARN} A newer version of kind is available: $LAST_VERSION (current: $VERSION)"
+else
+    echo "${ICON_OK} You are using the latest version of kind: $VERSION"
+fi
 
-echo "-------------------------------------------------------------------"
-echo "-- kind/install.sh - v$VERSION (latest=${LAST_VERSION})"
-echo "-------------------------------------------------------------------"
+
+echo "${ICON_INFO} Downloading kind v$VERSION to /usr/local/bin/kind ..."
 
 # For AMD64 / x86_64
-[ $(uname -m) = x86_64 ] && curl -Lo /tmp/kind https://kind.sigs.k8s.io/dl/v${VERSION}/kind-linux-amd64
+[ $(uname -m) = x86_64 ] && curl -sLo /tmp/kind https://kind.sigs.k8s.io/dl/v${VERSION}/kind-linux-amd64
 # For ARM64
-[ $(uname -m) = aarch64 ] && curl -Lo /tmp/kind https://kind.sigs.k8s.io/dl/v${VERSION}/kind-linux-arm64
+[ $(uname -m) = aarch64 ] && curl -sLo /tmp/kind https://kind.sigs.k8s.io/dl/v${VERSION}/kind-linux-arm64
 
 chmod +x /tmp/kind
 sudo cp /tmp/kind /usr/local/bin/.
-kind --version
+echo "${ICON_INFO} kind v$VERSION has been installed to /usr/local/bin/kind : $(kind --version)"
